64e268a54ce0eec9011d06ec699e72b7981afdd1
UnexomWid <uw@exom.dev>
2025-10-26 08:12:17 +0200

    remove: old files

--- Assets/Scripts/Tale/Scripts/Choice/ChoiceMaster.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace TaleUtil.Scripts
-{
-    public abstract class ChoiceMaster : MonoBehaviour
-    {
-        public abstract void Construct(object args, object choices, TaleUtil.Delegates.ShallowDelegate onEnd);
-    }
-}
